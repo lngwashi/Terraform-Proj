fe61f862f3cb86cdd8472ab15bf18c5b25601bb4
lngwashi <lengwash@gmail.com>
2023-03-18 14:54:33 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -8,12 +8,36 @@ terraform {
   }
 
 
-  backend "s3" {
-    bucket = "bootcamp30-01-lngwash"
-    key = "terraform/terraform.tfstate"
-    dynamodb_table = "terraform-lock"
-    region = "us-east-1"
+  resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "bootcamp30-$001-lngwash"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+
+
+   lifecycle {
+   prevent_destroy = false
+  }
 
  }
-}
+
+ # Provider Block
+provider "aws" {
+   region  = "us-east-1"
+   profile = "default"
+ }
+
 
